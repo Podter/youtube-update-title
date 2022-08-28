6663d35b61dc71b91562799a779d732b213576a5
Podter <48936015+Podter@users.noreply.github.com>
2022-08-28 21:55:14 +0700

    revoke (logout)

---
+++ scripts/logout.ts
@@ -0,0 +1,19 @@
+// logout
+
+import { google } from "googleapis";
+
+import clientSecret from "../client_secret.json";
+import token from "../user_credential.json";
+
+const oauth2Client = new google.auth.OAuth2(
+  clientSecret.web.client_id,
+  clientSecret.web.client_secret,
+  clientSecret.web.redirect_uris[0]
+);
+
+async function main() {
+  await oauth2Client.revokeToken(token.access_token);
+  console.log("Done! Don't forget to delete user_credential.json");
+}
+
+main();
