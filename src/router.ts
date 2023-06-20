e207a49e436f539eb9e73501d9ab902ea9869106
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-20 22:28:49 +0700

    removed boilerplate

--- src/router.ts
+++ src/router.ts
@@ -1,22 +1,9 @@
 import { Router } from 'itty-router';
 
-// now let's create a router (note the lack of "new")
 const router = Router();
 
-// GET collection index
-router.get('/api/todos', () => new Response('Todos Index!'));
+router.get('/', () => new Response('Hello world!'));
 
-// GET item
-router.get('/api/todos/:id', ({ params }) => new Response(`Todo #${params.id}`));
-
-// POST to the collection (we'll use async here)
-router.post('/api/todos', async (request) => {
-	const content = await request.json();
-
-	return new Response('Creating Todo: ' + JSON.stringify(content));
-});
-
-// 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }));
 
 export default router;
